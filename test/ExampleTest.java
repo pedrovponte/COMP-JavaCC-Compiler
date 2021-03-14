3bb8da86b2944ca83ec19f4c25b901d27c71be19
Mariana Ramos <7marianaramos@gmail.com>
2021-03-14 22:50:59 +0000

    tests fail and

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -70,4 +70,100 @@ public class ExampleTest {
         String jmmCode = SpecsIo.getResource("fixtures/public/WhileAndIf.jmm");
         TestUtils.parse(jmmCode);
     }
+
+    /*Fail semantic*/
+
+    @Test
+    public void testFail_arrindexnotint() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/arr_index_not_int.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_arrsizenotint() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/arr_size_not_int.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_badArguments() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/badArguments.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_binopincomp() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/bino_incomp.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_funcNotFound() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/funcNotFound.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_simpleLength() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/simple_length.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_varexpincomp() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/var_exp_incomp.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_litincomp() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/var_lit_incomp.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_varundef() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/var_undef.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_varNotInit() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/varNotInit.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    /*Fail syntactical*/
+
+
+    @Test
+    public void testFail_BlowUp() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/BlowUp.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_CompleteWhileTest() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/CompleteWhileTest.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_MissingRightPar() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/MissingRightPar.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_MultipleSequential() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/MultipleSequential.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
+    @Test
+    public void testFail_NestedLoop() {
+        String jmmCode = SpecsIo.getResource("fixtures/public/NestedLoop.jmm");
+        TestUtils.parse(jmmCode);
+    }
+
 }
