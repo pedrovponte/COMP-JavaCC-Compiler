a3f588f186602456780c409212134b8b5dc1d6f4
Mariana Ramos <7marianaramos@gmail.com>
2021-03-17 00:24:01 +0000

    ast cleaning

--- javacc/SimpleNode.java
+++ javacc/SimpleNode.java
@@ -103,7 +103,7 @@ class SimpleNode implements Node, JmmNode {
 
   public String toString() {
     switch (ParserTreeConstants.jjtNodeName[id]) {
-      case "Type": case "Class": case "Method": case "Identifier": case "Integer": case "Extends": case "InsideArray": case "Numeric": case "PrimitiveType":
+      case "Type": case "Class": case "Method": case "Identifier": case "Integer": case "Extends": case "InsideArray": case "Numeric": case "PrimitiveType": case "AfterIdentifier": case "AdditiveExpression":
         return ParserTreeConstants.jjtNodeName[id] + "[" + this.value + "]";
       default:
         return ParserTreeConstants.jjtNodeName[id];
