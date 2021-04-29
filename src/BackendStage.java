6d8b4ee7c24db89fe2907b5bd51b50488e5a7189
Pedro Ferreira <up201806506@fe.up.pt>
2021-04-29 21:59:25 +0100

    Assign

--- src/BackendStage.java
+++ src/BackendStage.java
@@ -69,65 +69,58 @@ public class BackendStage implements JasminBackend {
         }
     }
 
+    public void GetInstructions(Instruction inst, Method method){
+        switch (inst.getInstType()) {
+            case ASSIGN:
+                AssignInstruction assignInstruction = (AssignInstruction) inst;
+                jasmin.append("\n");
+
+                Instruction rhs = assignInstruction.getRhs();
+
+                GetInstructions(rhs,method);
+
+                Operand dest = (Operand) assignInstruction.getDest();
+
+                Descriptor descriptor = OllirAccesser.getVarTable(method).get(dest.getName());
+
+                jasmin.append("\tistore " + descriptor.getVirtualReg());
+
+                break;
+            case CALL:
+                break;
+            case GOTO:
+                break;
+            case BRANCH:
+                break;
+            case RETURN:
+                break;
+            case PUTFIELD:
+                break;
+            case GETFIELD:
+                break;
+            case UNARYOPER:
+                break;
+            case BINARYOPER:
+                //OperationType operation = ((UnaryOpInstruction) rhs).getUnaryOperation().getOpType();
+                /*switch (operation){
+                    case ADD:
+                        break;
+                    case SUB:
+                        break;
+                    case DIV:
+                        break;
+                    case MUL:
+                        break;
+                }*/
+                break;
+            case NOPER:
+                break;
+        }
+    }
+
     public void MethodOperations(Method method){
         for (var inst: method.getInstructions()) {
-            switch (inst.getInstType()) {
-                case ASSIGN:
-                    AssignInstruction assignInstruction = (AssignInstruction) inst;
-                    jasmin.append("\n");
-                    Operand dest = (Operand) assignInstruction.getDest();
-                    //jasmin.append("\taload_"+dest.getParamId()+"\n");
-
-                    Instruction rhs = assignInstruction.getRhs();
-                    //rhs.show();
-
-                    if (rhs instanceof SingleOpInstruction){
-                        jasmin.append("\taload_"+dest.getParamId()+"\n");
-                        jasmin.append("\tbipush ");
-                        SingleOpInstruction singleOperand = ((SingleOpInstruction) rhs);
-                        if (singleOperand.getSingleOperand().isLiteral()) {
-                            jasmin.append(((LiteralElement) singleOperand.getSingleOperand()).getLiteral());
-                        } else {
-                            Operand o1 = (Operand)singleOperand.getSingleOperand();
-                            System.out.println("\t" + singleOperand.getInstType() + " Operand: " + o1.getName() + " " + o1.getType());
-                        }
-                    }
-                    if (rhs instanceof BinaryOpInstruction){
-                        OperationType operation = ((UnaryOpInstruction) rhs).getUnaryOperation().getOpType();
-                        switch (operation){
-                            case ADD:
-                                break;
-                            case SUB:
-                                break;
-                            case DIV:
-                                break;
-                            case MUL:
-                                break;
-                        }
-                    }
-
-                    //System.out.println(dest.getParamId());
-
-                    break;
-                case CALL:
-                    break;
-                case GOTO:
-                    break;
-                case BRANCH:
-                    break;
-                case RETURN:
-                    break;
-                case PUTFIELD:
-                    break;
-                case GETFIELD:
-                    break;
-                case UNARYOPER:
-                    break;
-                case BINARYOPER:
-                    break;
-                case NOPER:
-                    break;
-            }
+            GetInstructions(inst, method);
         }
     }
 
